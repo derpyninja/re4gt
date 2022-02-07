7f62a808dd213bb7426f810b358046d6fd7253d1
Zaussinger  Felix <fzaussinger@ethz.ch>
2022-02-07 17:55:30 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project investigates the impact of the policy-driven transition to a climate neutral European economy on labour markets with a specific focus on the role of re- and upskilling.',
+    author='Felix Zaussinger, EPG, ETH Zurich',
+    license='MIT',
+)
